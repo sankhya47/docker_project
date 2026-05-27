6ffd9cbde158bb6827611eb6b7268a85cefd5fbf
sankhya47 <vivektendulkar07thala@gmail.com>
2026-05-27 19:06:22 +0530

    Update app.js to create notes

--- app.js
+++ app.js
@@ -1,11 +1,69 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
+app.use(express.json());
+
+const FILE = './data/notes.json';
+
+if (!fs.existsSync('./data')) {
+  fs.mkdirSync('./data');
+}
+
+if (!fs.existsSync(FILE)) {
+  fs.writeFileSync(FILE, JSON.stringify([]));
+}
+
+function readNotes() {
+  return JSON.parse(fs.readFileSync(FILE));
+}
+
+function writeNotes(notes) {
+  fs.writeFileSync(FILE, JSON.stringify(notes, null, 2));
+}
+
 app.get('/', (req, res) => {
-  res.send('Docker Updated!');
+  res.send('Docker Notes API Running');
+});
+
+app.get('/notes', (req, res) => {
+  const notes = readNotes();
+  res.json(notes);
+});
+
+app.post('/notes', (req, res) => {
+  const notes = readNotes();
+
+  const newNote = {
+    id: Date.now(),
+    text: req.body.text
+  };
+
+  notes.push(newNote);
+
+  writeNotes(notes);
+
+  res.json({
+    message: 'Note added',
+    note: newNote
+  });
+});
+
+app.delete('/notes/:id', (req, res) => {
+  const notes = readNotes();
+
+  const updated = notes.filter(
+    note => note.id != req.params.id
+  );
+
+  writeNotes(updated);
+
+  res.json({
+    message: 'Note deleted'
+  });
 });
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log('Notes API running on port 3000');
 });
